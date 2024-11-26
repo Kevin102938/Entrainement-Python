4f687eb724197a1ae1f7057f434be2a74048535e
GAC Kevin <kevin.gac@isoskele.fr>
2024-11-26 09:11:29 +0100

    dev de la loste

---
+++ liste.py
@@ -0,0 +1,9 @@
+#tableau
+liste_fruits = ["pomme", "banane", "orange"]
+print(liste_fruits)
+liste_fruits.append("kiwi")
+print(liste_fruits)
+liste_fruits.remove("orange")
+print(liste_fruits)
+liste_fruits[2] = "ananas"
+print(liste_fruits)
